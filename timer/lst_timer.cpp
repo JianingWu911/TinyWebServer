32e0ca96b58aecff01cde49e42b5dc5605049919
JianingWu911 <2909258534@qq.com>
2023-03-19 13:44:41 +0800

    定时器：从有序链表升级为时间轮

--- timer/lst_timer.cpp
+++ timer/lst_timer.cpp
@@ -1,149 +1,261 @@
 #include "lst_timer.h"
 #include "../http/http_conn.h"
 
-sort_timer_lst::sort_timer_lst()
-{
-    head = NULL;
-    tail = NULL;
-}
-sort_timer_lst::~sort_timer_lst()
-{
-    util_timer *tmp = head;
-    while (tmp)
-    {
-        head = tmp->next;
-        delete tmp;
-        tmp = head;
+// sort_timer_lst::sort_timer_lst()
+// {
+//     head = NULL;
+//     tail = NULL;
+// }
+// sort_timer_lst::~sort_timer_lst()
+// {
+//     util_timer *tmp = head;
+//     while (tmp)
+//     {
+//         head = tmp->next;
+//         delete tmp;
+//         tmp = head;
+//     }
+// }
+tw_timer::tw_timer(int rot, int ts) : prev(nullptr), \
+    next(nullptr), rotation(rot), time_slot(ts){};
+
+time_wheel::time_wheel() : cur_slot(0) {
+    for (int i = 0; i < N; ++i) {
+        slots[i] = nullptr;
     }
 }
 
-void sort_timer_lst::add_timer(util_timer *timer)
-{
-    if (!timer)
-    {
-        return;
+time_wheel::~time_wheel() {
+    for (int i = 0; i < N; ++i) {
+        tw_timer *tmp = slots[i]; // 将每个槽的所有节点删除掉
+        while (tmp) {
+            slots[i] = tmp->next;
+            delete tmp;
+            tmp = slots[i]->next;
+        }
     }
-    if (!head)
-    {
-        head = tail = timer;
-        return;
-    }
-    if (timer->expire < head->expire)
-    {
-        timer->next = head;
-        head->prev = timer;
-        head = timer;
-        return;
-    }
-    add_timer(timer, head);
 }
-void sort_timer_lst::adjust_timer(util_timer *timer)
-{
-    if (!timer)
-    {
-        return;
+
+tw_timer* time_wheel::add_timer(int timeout) {
+    if(timeout < 0) return nullptr;
+    int ticks = 0;
+    if (timeout < SI) ticks = 1;
+    else ticks = timeout / SI;
+    int rotation = ticks / N; // 计算需要转多少圈
+    int ts = (cur_slot + ticks % N) % N; // 计算应该插入哪个槽
+    tw_timer *timer = new tw_timer(rotation, ts); 
+    if (nullptr != slots[ts]) {
+        timer->next = slots[ts];
+        slots[ts]->prev = timer;
+        slots[ts] = timer;
     }
-    util_timer *tmp = timer->next;
-    if (!tmp || (timer->expire < tmp->expire))
-    {
-        return;
+    else {
+        slots[ts] = timer;
     }
-    if (timer == head)
-    {
-        head = head->next;
-        head->prev = NULL;
-        timer->next = NULL;
-        add_timer(timer, head);
+    return timer; // 为什么要返回这个指针呢？
+}
+
+void time_wheel::adjust_timer(tw_timer *timer, int time_slot) {
+    if (nullptr == timer) return;
+    int diff_rotation = (time_slot) / N; // rotation 的差值
+    int new_slot = (cur_slot + (time_slot) % N) % N; //修改后的slot
+    int old_slot = timer->time_slot; // 修改前的slot
+    if (timer == slots[old_slot]) { // 如果是第一个元素
+        slots[old_slot] = timer->next;
+        if (nullptr != slots[old_slot]) slots[old_slot]->prev = nullptr;
     }
-    else
-    {
+    else { // 不是第一个元素
         timer->prev->next = timer->next;
-        timer->next->prev = timer->prev;
-        add_timer(timer, timer->next);
+        if (nullptr != timer->next) timer->next->prev = timer->prev;
     }
-}
-void sort_timer_lst::del_timer(util_timer *timer)
-{
-    if (!timer)
-    {
-        return;
+    // 更新信息
+    timer->rotation += diff_rotation;
+    timer->time_slot = new_slot;
+    timer->next = nullptr;
+    timer->prev = nullptr;
+    if (nullptr == slots[new_slot]) { // 为空，直接放入
+        slots[new_slot] = timer;
     }
-    if ((timer == head) && (timer == tail))
-    {
-        delete timer;
-        head = NULL;
-        tail = NULL;
-        return;
-    }
-    if (timer == head)
-    {
-        head = head->next;
-        head->prev = NULL;
-        delete timer;
-        return;
-    }
-    if (timer == tail)
-    {
-        tail = tail->prev;
-        tail->next = NULL;
-        delete timer;
-        return;
-    }
-    timer->prev->next = timer->next;
-    timer->next->prev = timer->prev;
-    delete timer;
-}
-void sort_timer_lst::tick()
-{
-    if (!head)
-    {
-        return;
-    }
-    
-    time_t cur = time(NULL);
-    util_timer *tmp = head;
-    while (tmp)
-    {
-        if (cur < tmp->expire)
-        {
-            break;
-        }
-        tmp->cb_func(tmp->user_data);
-        head = tmp->next;
-        if (head)
-        {
-            head->prev = NULL;
-        }
-        delete tmp;
-        tmp = head;
+    else {// 非空，插入头部
+        timer->next = slots[new_slot];
+        slots[new_slot]->prev = timer;
+        slots[new_slot] = timer;
     }
 }
 
-void sort_timer_lst::add_timer(util_timer *timer, util_timer *lst_head)
-{
-    util_timer *prev = lst_head;
-    util_timer *tmp = prev->next;
-    while (tmp)
-    {
-        if (timer->expire < tmp->expire)
-        {
-            prev->next = timer;
-            timer->next = tmp;
-            tmp->prev = timer;
-            timer->prev = prev;
-            break;
-        }
-        prev = tmp;
-        tmp = tmp->next;
+void time_wheel::del_timer(tw_timer *timer) {
+    if (nullptr == timer) return;
+    int ts = timer->time_slot;
+    // 如果是第一个元素，需要处理slots[ts]
+    if (timer == slots[ts]) {
+        slots[ts] = timer->next;
+        if (nullptr != slots[ts]) slots[ts]->prev = nullptr;
+        delete timer;
+        return;
     }
-    if (!tmp)
-    {
-        prev->next = timer;
-        timer->prev = prev;
-        timer->next = NULL;
-        tail = timer;
-    }
+    // 不是第一个元素，不需要处理slots[ts];
+    timer->prev->next = timer->next;
+    if (nullptr != timer->next) timer->next->prev = timer->prev; // 如果非空，处理
+    delete timer; 
 }
+void time_wheel::tick() {
+    tw_timer *tmp = slots[cur_slot];
+    while (nullptr != tmp) {
+        if (tmp->rotation > 1) {
+            --(tmp->rotation);
+            tmp = tmp->next;
+        }
+        else {
+            tmp->cb_func(tmp->user_data);
+            // 是第一个元素
+            if (slots[cur_slot] == tmp) {
+                slots[cur_slot] = tmp->next;
+                delete tmp;
+                if(nullptr != slots[cur_slot]) slots[cur_slot]->prev = nullptr;
+                tmp = slots[cur_slot];
+            }
+            // 不是第一个元素
+            else {
+                tw_timer *cur = tmp->next; // 用来记录下一个
+                tmp->prev->next = tmp->next;
+                if (nullptr != tmp->next) tmp->next->prev = tmp->prev;
+                delete tmp;
+                tmp = cur;
+            }
+        }
+    }
+    cur_slot = ++cur_slot % N;
+}
+
+
+
+// void sort_timer_lst::add_timer(util_timer *timer)
+// {
+//     if (!timer)
+//     {
+//         return;
+//     }
+//     if (!head)
+//     {
+//         head = tail = timer;
+//         return;
+//     }
+//     if (timer->expire < head->expire)
+//     {
+//         timer->next = head;
+//         head->prev = timer;
+//         head = timer;
+//         return;
+//     }
+//     add_timer(timer, head);
+// }
+// void sort_timer_lst::adjust_timer(util_timer *timer)
+// {
+//     if (!timer)
+//     {
+//         return;
+//     }
+//     util_timer *tmp = timer->next;
+//     if (!tmp || (timer->expire < tmp->expire))
+//     {
+//         return;
+//     }
+//     if (timer == head)
+//     {
+//         head = head->next;
+//         head->prev = NULL;
+//         timer->next = NULL;
+//         add_timer(timer, head);
+//     }
+//     else
+//     {
+//         timer->prev->next = timer->next;
+//         timer->next->prev = timer->prev;
+//         add_timer(timer, timer->next);
+//     }
+// }
+// void sort_timer_lst::del_timer(util_timer *timer)
+// {
+//     if (!timer)
+//     {
+//         return;
+//     }
+//     if ((timer == head) && (timer == tail))
+//     {
+//         delete timer;
+//         head = NULL;
+//         tail = NULL;
+//         return;
+//     }
+//     if (timer == head)
+//     {
+//         head = head->next;
+//         head->prev = NULL;
+//         delete timer;
+//         return;
+//     }
+//     if (timer == tail)
+//     {
+//         tail = tail->prev;
+//         tail->next = NULL;
+//         delete timer;
+//         return;
+//     }
+//     timer->prev->next = timer->next;
+//     timer->next->prev = timer->prev;
+//     delete timer;
+// }
+// void sort_timer_lst::tick()
+// {
+//     if (!head)
+//     {
+//         return;
+//     }
+    
+//     time_t cur = time(NULL);
+//     util_timer *tmp = head;
+//     while (tmp)
+//     {
+//         if (cur < tmp->expire)
+//         {
+//             break;
+//         }
+//         tmp->cb_func(tmp->user_data);
+//         head = tmp->next;
+//         if (head)
+//         {
+//             head->prev = NULL;
+//         }
+//         delete tmp;
+//         tmp = head;
+//     }
+// }
+
+// void sort_timer_lst::add_timer(util_timer *timer, util_timer *lst_head)
+// {
+//     util_timer *prev = lst_head;
+//     util_timer *tmp = prev->next;
+//     while (tmp)
+//     {
+//         if (timer->expire < tmp->expire)
+//         {
+//             prev->next = timer;
+//             timer->next = tmp;
+//             tmp->prev = timer;
+//             timer->prev = prev;
+//             break;
+//         }
+//         prev = tmp;
+//         tmp = tmp->next;
+//     }
+//     if (!tmp)
+//     {
+//         prev->next = timer;
+//         timer->prev = prev;
+//         timer->next = NULL;
+//         tail = timer;
+//     }
+// }
 
 void Utils::init(int timeslot)
 {
@@ -201,7 +313,7 @@ void Utils::addsig(int sig, void(handler)(int), bool restart)
 //定时处理任务，重新定时以不断触发SIGALRM信号
 void Utils::timer_handler()
 {
-    m_timer_lst.tick();
+    t_wheel.tick();
     alarm(m_TIMESLOT);
 }
 
--- timer/lst_timer.h
+++ timer/lst_timer.h
@@ -24,46 +24,71 @@
 #include <time.h>
 #include "../log/log.h"
 
-class util_timer;
+class tw_timer;
 
 struct client_data
 {
     sockaddr_in address;
     int sockfd;
-    util_timer *timer;
+    tw_timer *timer;
 };
 
-class util_timer
-{
-public:
-    util_timer() : prev(NULL), next(NULL) {}
+// class util_timer
+// {
+// public:
+//     util_timer() : prev(NULL), next(NULL) {}
 
-public:
-    time_t expire;
+// public:
+//     time_t expire;
     
+//     void (* cb_func)(client_data *);
+//     client_data *user_data;
+//     util_timer *prev;
+//     util_timer *next;
+// };
+
+class tw_timer {
+public:
+    tw_timer(int rot, int ts);
+    int rotation; // 记录多少圈(相对圈数）后生效
+    int time_slot; // 记录属于哪个slot ？？？
     void (* cb_func)(client_data *);
     client_data *user_data;
-    util_timer *prev;
-    util_timer *next;
+    tw_timer* prev;
+    tw_timer* next;
 };
 
-class sort_timer_lst
-{
-public:
-    sort_timer_lst();
-    ~sort_timer_lst();
-
-    void add_timer(util_timer *timer);
-    void adjust_timer(util_timer *timer);
-    void del_timer(util_timer *timer);
-    void tick();
-
+class time_wheel {
 private:
-    void add_timer(util_timer *timer, util_timer *lst_head);
-
-    util_timer *head;
-    util_timer *tail;
+    static const int N = 60; // 共有60个槽
+    static const int SI = 1; //时间间隔为1s
+    tw_timer* slots[N]; // 每个槽都是一个链表
+    int cur_slot; // 当前槽
+public:
+    time_wheel();
+    ~time_wheel();
+    tw_timer* add_timer(int timeout);
+    void del_timer(tw_timer *timer);
+    void adjust_timer(tw_timer *timer, int time_slot); // 向后延迟time_slot
+    void tick();
 };
+// class sort_timer_lst
+// {
+// public:
+//     sort_timer_lst();
+//     ~sort_timer_lst();
+
+//     void add_timer(util_timer *timer);
+//     void adjust_timer(util_timer *timer);
+//     void del_timer(util_timer *timer);
+//     void tick();
+
+// private:
+//     void add_timer(util_timer *timer, util_timer *lst_head);
+
+//     util_timer *head;
+//     util_timer *tail;
+// };
 
 class Utils
 {
@@ -92,7 +117,7 @@ public:
 
 public:
     static int *u_pipefd;
-    sort_timer_lst m_timer_lst;
+    time_wheel t_wheel; 
     static int u_epollfd;
     int m_TIMESLOT;
 };
